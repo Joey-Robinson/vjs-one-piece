bf7578525f91b604cd4a2356a39676364bbdc244
Joey Robinson <theamazingness@gmail.com>
2019-03-31 15:37:35 -0400

    Adding comments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+// Importing .env file which contains port location
 require('dotenv').config()
+
+// Import express and setting it to app variable
 const express = require('express')
 const app = express()
 
+// Importing routes and assiging them to variables
 const indexPath = require('./routes/index')
 const aboutPath = require('./routes/about')
 const contactPath = require('./routes/contact')
 
+// Consuming route variables
 app.use(indexPath)
 app.use(aboutPath)
 app.use(contactPath)
 
+// Setting public folder for static content (Images, CSS, and JS)
 app.use('/static', express.static('public'))
 
+// Setting listening port from the env file
 app.listen(process.env.PORT)
